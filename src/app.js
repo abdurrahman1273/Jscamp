ffcaaf4dbfeb8fee21143e073a443da640007267
abdurrahman1273 <acalis999@gmail.com>
2021-10-19 00:02:37 +0300

    18 Ekim Ders sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba kodlamaio")
+
+let dolarBugun = 9.20
+
+let dolarDun = 9.30
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+//array
+let konutKredileri =["konut kredisi","Emlak konut kredisi","kamu kredileri"]
+
+console.log("<ul>")
+for(let i = 0;i<3;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
+
+//console.log(konutKredileri)
