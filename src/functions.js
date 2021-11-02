6a8443f9d933fcaa29970af2a05aaacd4a860def
abdurrahman1273 <acalis999@gmail.com>
2021-11-03 02:23:42 +0300

    fist

--- src/functions.js
+++ src/functions.js
@@ -1,17 +1,4 @@
 function addToCart(quantity,productName="Elma") {
-<<<<<<< HEAD
-    console.log("sepete eklendi : ürün : "
-     + productName + " adet : " +quantity)
-    
-}
-// addToCart()
-addToCart(10)
-// addToCart(15)
-
-
-let sayHello = ()=>{
-    console.log("Hello world!")
-=======
     console.log("Sepete eklendi : ürün : " 
     + productName + " adet : " + quantity)
 }
@@ -22,73 +9,23 @@ addToCart(10)
 
 let sayHello = ()=>{
     console.log("Hello World!")
->>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 }
 
 sayHello()
 
 let sayHello2 = function () {
-<<<<<<< HEAD
-    console.log("Hello world 2")   
-}
-sayHello2();
 
-function addToCart2(productName, quantity, unitPrice) {
-
-=======
     console.log("Hello World 2")
 }
 
 sayHello2();
 
 function addToCart2(productName, quantity, unitPrice) {
->>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
-    
+
+
 }
 
 addToCart2("Elma",5,10)
-<<<<<<< HEAD
-addToCart2("Armut",2,20)
-addToCart2("Limon",3,8)
-
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
-
-    function addToCart3(product) {
-        console.log("Ürün : "+product.productName)
-        console.log("Adet : "+product.quantity)
-        console.log("Fiyat : "+product.unitPrice)
-    }
-    
-    addToCart3(product1)
-
-    let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-    let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-    product2=product3
-    product2.productName="KARPUZ"
-    console.log(product3.productName)
-
-
-    let sayi1=10
-    let sayi2=20
-    sayi1=sayi2
-    sayi2=100
-    console.log(sayi1)
-
-
-
-    function addToCart4(products) {
-        console.log(products)
-        
-    }
-
-    let  products = [
-        {productName:"Elma", unitPrice:10, quantity:5},
-        {productName:"Armut", unitPrice:10, quantity:5},
-        {productName:"Karpuz", unitPrice:10, quantity:5}
-    ]
-    addToCart4(products)
-    
-=======
 addToCart2("Armut", 2, 20)
 addToCart2("Limon",3,15)
 
@@ -167,4 +104,3 @@ console.log(newProductName)
 console.log(newUnitPrice)
 console.log(newQuantity)
 
->>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
